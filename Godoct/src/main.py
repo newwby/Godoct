d5024d5a31e8a7356ea69b224472df233928c692
DanDoesAThing <60653155+newwby@users.noreply.github.com>
2024-08-07 15:43:32 +0100

    parser folded into main

--- Godoct/src/main.py
+++ Godoct/src/main.py
@@ -1,9 +1,13 @@
 import os
 import re
-import parser
+import parsefunc
+import parsevar
 
 GODOCT_DOCS_DIRECTORY = "docs"
 GODOCT_INCLUDE_FILE_NAME = "godoct_include.txt"
+#TODO REMOVE LATER
+TEST_FILE_PATH = "C:\\Users\\Daniel\\PycharmProjects\\Godoct\\Godoct\\src\\test_file.gd"
+
 
 # finds all .gd files within the same directory
 def get_all_gdscript_paths():
@@ -62,6 +66,40 @@ def get_own_directory():
     return os.path.dirname(script_path)
 
 
-if __name__ == "__main__":
-    valid_paths = get_matched_gdscripts(get_included_file_names(), get_all_gdscript_paths())
-    parser.parse_gdscript_file(parser.TEST_FILE_PATH)
+# should be passed a path to a gdscript file
+# returns a dict of the contents structured as follows
+"""{
+    "script_name": a string representing the script file name,
+    "class_parent": a string representing the extends attribute,
+    "class_name": a string representing the class_name attribute,
+    "class_documentation": a string representing documentation comments at the top of the script,
+    "signals": a list of nested dictionaries where each dictionary details a signal in the script,
+    "properties": a list of nested dictionaries where each dictionary details a property (var/const/enum) in the script,
+    "functions": a list of nested dictionaries where each dictionary details a function in the script,
+}"""
+# with the exception of the 'script_name' property, the values to these keys can be empty (nil strings or empty lists)
+def parse_gdscript_file(arg_gdscript_file_path):
+    output_structure = {
+        "script_name": "",
+        "class_parent": "",
+        "class_name": "",
+        "class_documentation": "",
+        "signals": [],
+        "properties": [],
+        "functions": [],
+    }
+    try:
+        gdfile = open(arg_gdscript_file_path)
+    except:
+        print("file open error")
+        return
+
+    function_output = parsefunc.find_function_data(gdfile) #parse_functions(find_functions())
+
+    # TESTING ONLY
+    for i in function_output:
+        print(i)
+
+
+valid_paths = get_matched_gdscripts(get_included_file_names(), get_all_gdscript_paths())
+parse_gdscript_file(TEST_FILE_PATH)
